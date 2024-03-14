c1529bc6502c9895d1c5ccc4d897862f66006122
Vanessa Pérez Escorcia <79584378+vane005@users.noreply.github.com>
2024-03-14 00:20:31 -0600

    Add files via upload

---
+++ Métodos de solución de Sistemas de Ecuaciones/Eliminacion_Gaussiana/src/ejercicio5.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vanee
+ */
+public class ejercicio5 {
+    // Matriz aumentada [A | B]
+    static double[][] augmentedMatrix = {
+        {1, 5, 6, 4},
+        {5, 6, 7, 8},
+        {9, 0, 7, 12}
+    };
+    static void gaussianElimination(int row, int col) {
+        if (row == augmentedMatrix.length - 1) {
+            return;
+        }
+        if (augmentedMatrix[row][col] == 0) {
+            for (int i = row + 1; i < augmentedMatrix.length; i++) {
+                if (augmentedMatrix[i][col] != 0) {
+                    double[] temp = augmentedMatrix[i];
+                    augmentedMatrix[i] = augmentedMatrix[row];
+                    augmentedMatrix[row] = temp;
+                    break;
+                }
+            }
+        }
+        for (int i = row + 1; i < augmentedMatrix.length; i++) {
+            double factor = augmentedMatrix[i][col] / augmentedMatrix[row][col];
+            for (int j = col; j < augmentedMatrix[row].length; j++) {
+                augmentedMatrix[i][j] -= factor * augmentedMatrix[row][j];
+            }
+        }
+        gaussianElimination(row + 1, col + 1);
+    }
+    
+    public static void main(String[] args) {
+        gaussianElimination(0, 0);
+        System.out.println("Matriz triangular superior:");
+        for (int i = 0; i < augmentedMatrix.length; i++) {
+            for (int j = 0; j < augmentedMatrix[i].length; j++) {
+                System.out.print(augmentedMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Métodos de solución de Sistemas de Ecuaciones/Gauss_Saidel/src/GaussSaidel.java
@@ -0,0 +1,50 @@
+
+public class GaussSaidel {
+    public static void main(String[] args) {
+        double[][] matrizCoeficientes = {{4, 1, -1}, {2, 7, 1}, {1, 3, 5}};
+        double[] vectorTerminosIndependientes = {8, 3, 14};
+        int iteracionesMaximas = 100;
+        double tolerancia = 0.0001;
+
+        double[] solucion = resolverMetodoGaussSeidel(matrizCoeficientes, vectorTerminosIndependientes, new double[vectorTerminosIndependientes.length], iteracionesMaximas, tolerancia, 0);
+
+        System.out.println("Solución:");
+        for (int i = 0; i < solucion.length; i++) {
+            System.out.println("x[" + i + "] = " + solucion[i]);
+        }
+    }
+
+    public static double[] resolverMetodoGaussSeidel(double[][] matrizCoeficientes, double[] vectorTerminosIndependientes,
+                                                     double[] solucionActual, int iteracionesMaximas, double tolerancia, int iteracion) {
+        int n = matrizCoeficientes.length;
+
+        double[] nuevaSolucion = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            double suma = 0.0;
+            for (int j = 0; j < n; j++) {
+                if (j != i) {
+                    suma += matrizCoeficientes[i][j] * (iteracion == 0 ? 0 : nuevaSolucion[j]);
+                }
+            }
+            nuevaSolucion[i] = (vectorTerminosIndependientes[i] - suma) / matrizCoeficientes[i][i];
+        }
+        double error = calcularError(nuevaSolucion, solucionActual);
+        if (error < tolerancia || iteracion >= iteracionesMaximas) {
+            return nuevaSolucion;
+        }
+        return resolverMetodoGaussSeidel(matrizCoeficientes, vectorTerminosIndependientes, nuevaSolucion, iteracionesMaximas, tolerancia, iteracion + 1);
+    }
+
+    private static double calcularError(double[] solucionActual, double[] solucionAnterior) {
+        double maxError = 0.0;
+        for (int i = 0; i < solucionActual.length; i++) {
+            double error = Math.abs(solucionActual[i] - solucionAnterior[i]);
+            if (error > maxError) {
+                maxError = error;
+            }
+        }
+        return maxError;
+    }
+}
+
